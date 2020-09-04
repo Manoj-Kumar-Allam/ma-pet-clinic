432563919461119935b90565869758a968ef9ed7
Manoj Kumar Allam <Manoj.Allam@kpit.com>
2020-09-04 15:34:50 +0530

    Implemented closes #27

---
+++ pet-clinic-data/src/main/java/com/ma/petclinicdata/model/Specialty.java
@@ -0,0 +1,16 @@
+package com.ma.petclinicdata.model;
+
+public class Specialty {
+	
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
--- pet-clinic-data/src/main/java/com/ma/petclinicdata/model/Vet.java
+++ pet-clinic-data/src/main/java/com/ma/petclinicdata/model/Vet.java
@@ -1,10 +1,22 @@
 package com.ma.petclinicdata.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	Set<Specialty> specialties;
+
+	public Set<Specialty> getSpecialties() {
+		return specialties;
+	}
+
+	public void setSpecialties(Set<Specialty> specialties) {
+		this.specialties = specialties;
+	}
 
 }
