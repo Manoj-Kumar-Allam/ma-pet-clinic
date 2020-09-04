bb3b720a55d4d8550c862b08e86297297d99f275
Manoj Kumar Allam <Manoj.Allam@kpit.com>
2020-09-04 17:02:04 +0530

    Closes #45

--- pet-clinic-web/src/main/java/com/ma/petclinicweb/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ma/petclinicweb/controllers/OwnerController.java
@@ -22,5 +22,10 @@ public class OwnerController {
 		model.addAttribute("owners", this.ownerService.findAll());
 		return "owners/index";
 	}
+	
+	@RequestMapping("/find")
+	public String find() {
+		return "notimplemented";
+	}
 
 }
