6d36dab8213ca986e09d3085db64fdf2c8fe7b17
Manoj Kumar Allam <Manoj.Allam@kpit.com>
2020-09-04 15:28:56 +0530

    Implemented Visit Entity closes and updated Owners with Pet  ref #26

--- pet-clinic-data/src/main/java/com/ma/petclinicdata/model/Owner.java
+++ pet-clinic-data/src/main/java/com/ma/petclinicdata/model/Owner.java
@@ -1,10 +1,24 @@
 package com.ma.petclinicdata.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
+	
+	
 
 }
