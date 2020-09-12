cd3aff5216f3a1051c65ab009bdadc1538cd1a8f
Manoj Kumar Allam <Manoj.Allam@kpit.com>
2020-09-12 23:28:40 +0530

    closes #57

--- pet-clinic-web/src/main/java/com/ma/petclinicweb/controllers/PetController.java
+++ pet-clinic-web/src/main/java/com/ma/petclinicweb/controllers/PetController.java
@@ -6,7 +6,6 @@ import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
